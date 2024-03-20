9f8114fb7d477bc49c41024a9d2b533623893d29
nikolayhotim1 <hotim_nikolay@mail.ru>
2024-03-20 23:52:55 +0300

    feature: marketing page

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,18 @@
+import { Footer } from './footer'
+import { Header } from './header'
+
+interface MarketingLayoutProps {
+	children: React.ReactNode
+}
+
+export default function MarketingLayout({ children }: MarketingLayoutProps) {
+	return (
+		<div className='min-h-screen flex flex-col'>
+			<Header />{' '}
+			<main className='flex-1 flex flex-col items-center justify-center'>
+				{children}
+			</main>
+			<Footer />
+		</div>
+	)
+}
