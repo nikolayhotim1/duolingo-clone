7caa16a5d3f66541e29e56a74ff0b4d2c4ae718b
nikolayhotim1 <hotim_nikolay@mail.ru>
2024-03-27 23:52:25 +0300

    feature: learn page wrappers

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,3 +1,22 @@
+import { FeedWrapper } from '@/components/feed-wrapper'
+import { StickyWrapper } from '@/components/sticky-wrapper'
+import { Header } from './header'
+import { UserProgress } from '@/components/user-progress'
+
 export default function LearnPage() {
-	return <div>LearnPage</div>
+	return (
+		<div className='flex flex-row-reverse gap-[48px] px-6'>
+			<StickyWrapper>
+				<UserProgress
+					activeCourse='{userProgress.activeCourse}'
+					hearts={5}
+					points={100}
+					hasActiveSubscription={true}
+				/>
+			</StickyWrapper>
+			<FeedWrapper>
+				<Header title='{userProgress.activeCourse.title}' />
+			</FeedWrapper>
+		</div>
+	)
 }
---
+++ components/feed-wrapper.tsx
@@ -0,0 +1,3 @@
+export function FeedWrapper({ children }: { children: React.ReactNode }) {
+	return <div className='flex-1 relative top-0 pb-10'>{children}</div>
+}
